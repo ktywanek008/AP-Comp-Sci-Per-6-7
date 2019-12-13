d178600d26f376d801d0cfbb2d7609b2bcda2797
ktywanek008 <58831890+ktywanek008@users.noreply.github.com>
2019-12-12 20:14:52 -0500

    Update ATM.java
    Integrated with Main.java and fixed a NoSuchElementExecption when trying to withdraw from ATM

--- ATM.java
+++ ATM.java
@@ -1,4 +1,5 @@
 import java.time.LocalDate;
+import java.util.Scanner;
 class ATM {
 	//default constructor
 	public ATM() {
@@ -15,6 +16,7 @@ class ATM {
 		accountName = input_accountName;
 		cardNumber = 0;
 		pin = input_pin;
+		verified = false;
 	}
 	public ATM(String input_location, String input_managedBy, int input_cardNumber, int input_pin) {
 		location = input_location;
@@ -22,6 +24,7 @@ class ATM {
 		accountName = "";
 		cardNumber = input_cardNumber;
 		pin = input_pin;
+		verified = false;
 	}
 	//accessor methods
 	public String getLocation() {
@@ -56,10 +59,13 @@ class ATM {
 		pin = input_pin;
 	}
 	//methods
-	public void deposit(int amount) {
-		accountBalance += amount;
+	public void deposit() {
+		vito.checkingdeposit();
 	}
-	public int withdraw(int amount) {
+	public int withdraw() {
+		//ui: get withdraw amount
+		System.out.println("How much do you want to withdraw?");
+		int amount = new Scanner(System.in).nextInt();//
 		//check date: if date is different than the initial value, reset the daily amount
 		if (!date.equalsIgnoreCase(LocalDate.now().toString())) {
 			date = LocalDate.now().toString();
@@ -67,7 +73,7 @@ class ATM {
 		}
 		//check managedBy: if a different company's atm is used, add a $5 charge
 		int charge = 0;
-		if (!managedBy.equalsIgnoreCase("Bank Name")) {
+		if (!managedBy.equalsIgnoreCase("The Worst Bank")) {
 			charge = 5;
 		}
 		//check dailyAmount: don't return money if there is no money left in the atm
@@ -77,9 +83,9 @@ class ATM {
 			return 0;
 		}
 		//check accountBalance: make sure there is enough money in the account, this is the "correct" output
-		if ((amount + charge) < accountBalance) {
+		if ((amount + charge) < vito.getcheckingBalance()) {
 			if (amount < dailyAmount) {
-				accountBalance -= (amount + charge);
+				vito.checkingwithdraw(amount + charge);
 				//System.out.println("balance = " + accountBalance);
 				dailyAmount -= amount;
 				return amount;
@@ -88,7 +94,7 @@ class ATM {
 			else {
 				System.out.println("There are insufficient funds in this ATM.");
 				int temp = dailyAmount;
-				accountBalance -= (dailyAmount + charge);
+				vito.checkingwithdraw(dailyAmount + charge);
 				dailyAmount = 0;
 				//System.out.println("balance = " + accountBalance);
 				return temp;
@@ -97,18 +103,60 @@ class ATM {
 		//output for when there is not enough money in the account, returns as much money as the account has
 		else {
 			System.out.println("There are insufficient funds in this account.");
-			int temp = accountBalance;
-			accountBalance = 0;
+			int temp = (int) vito.getcheckingBalance();
+			Main.vito.checkingwithdraw(vito.getcheckingBalance());
 			return temp;
 		}
 	}
+	public void checkVerification() {
+		//Won't work until Account team gets customer information
+		if (cardNumber == 0) {
+			if (accountName.equals(vito1.getAccountName()) && pin == vito1.getPin()) {
+				verified = true;
+			}
+			else {
+				System.out.println("Account information is incorrect, please re-enter it and try again.");
+				Scanner input = new Scanner(System.in);
+				System.out.println("Enter account name:");
+				String input_accountName = input.nextLine();
+				this.setAccountName(input_accountName);
+				System.out.println("Enter pin number:");
+				int input_pin = input.nextInt();
+				this.setPin(input_pin);
+				//input.close();
+			}
+		}
+		else {
+			if (cardNumber == vito1.getCardNumber() && pin == vito1.getPin()) {
+				verified = true;
+			}
+			else {
+				System.out.println("Account information is incorrect, please re-enter it and try again.");
+				Scanner input = new Scanner(System.in);
+				System.out.println("Enter account name:");
+				int input_cardNumber = input.nextInt();
+				this.setCardNumber(input_cardNumber);
+				System.out.println("Enter pin number:");
+				int input_pin = input.nextInt();
+				this.setPin(input_pin);
+				//input.close();
+				//DO NOT close a scanner then make more scanners
+				//since closing a Scanner also closes System.in and prevents making more
+				//because a NoSuchElementException is thrown since System.in is not found when calling
+				//new Scanner(System.in)
+				
+			}
+		}
+	}
 	//variables
+	BankAccount vito = Main.vito;
+	Customer vito1 = Main.vito1;
+	boolean verified;
 	String location;
 	String managedBy;
 	String accountName;
 	int cardNumber;
 	int pin;
 	int dailyAmount = 1000;
-	int accountBalance = 10000;
 	String date = LocalDate.now().toString();
 }
