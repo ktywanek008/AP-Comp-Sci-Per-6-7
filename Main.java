9983410dd5577ccd687ab9b34e2cbe04ba095e94
ktywanek008 <58831890+ktywanek008@users.noreply.github.com>
2019-12-12 20:22:19 -0500

    Update Main.java
    Integrated with ATM.java and proves that it works

--- Main.java
+++ Main.java
@@ -1,9 +1,18 @@
+
 import java.util.Scanner;
 public class Main {
-    
+	//Making the objects static allows access from other classes using
+	static Customer vito1 = new Customer(); 
+	static BankAccount vito = new BankAccount();
+	static ATM atm = new ATM("a","The Worst Bank",0,0);
+	
     public static void main(String[] args) {
-        
-    BankAccount vito = new BankAccount();
-    Customer vito1 = new Customer();
+    	
+    	vito.checkingdeposit();//Make a deposit to the checking account
+        System.out.println(vito.getcheckingBalance());//Show that the deposit worked
+       atm.checkVerification();
+       atm.withdraw();//Use the atm to withdraw from the checking account
+       System.out.println(vito.getcheckingBalance());//Bank Account shows new balance
+    
     }
 }
